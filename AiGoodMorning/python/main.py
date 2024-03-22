44aaea48e5c28a53568d3a9958163bae46a4da18
AdrianHsu <adrianhsu1995@gmail.com>
2024-03-22 00:46:08 -0700

    add photos

--- AiGoodMorning/python/main.py
+++ AiGoodMorning/python/main.py
@@ -7,13 +7,14 @@ from openai import OpenAI
 
 client = OpenAI(api_key=api_key)
 
-response = client.images.generate(
-  model="dall-e-2",
-  prompt="我想要喜氣洋洋、有鞭炮跟舞龍舞獅",
-  size="256x256",
-  quality="standard",
-  n=1,
-)
+# response = client.images.generate(
+#   model="dall-e-2",
+#   prompt="我想要喜氣洋洋、有鞭炮跟舞龍舞獅",
+#   size="256x256",
+#   quality="standard",
+#   n=1,
+# )
 
-image_url = response.data[0].url
+image_url = "url_output"
+# image_url = response.data[0].url
 print(image_url)
